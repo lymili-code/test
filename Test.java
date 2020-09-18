98376cbc36d1d6d1a7b5fdebc0f3556eed2d354a
xiaomili <71482630+lymili-code@users.noreply.github.com>
2020-09-18 15:53:59 +0800

    修改Test.java文件
    第一次使用GitHub修改文件，练习一把

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 public calss Test{
           public static void main{String[] args){
-                     System.out.print(“hello world”);    
+                     System.out.print("hello world"); 
+                     System.out.print("my first java");
+                    
       }
 }
